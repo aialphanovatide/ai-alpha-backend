7c54e5ffb0aad090ff1972196a1da6ca604cc9f4
daviddflix <david-972010@hotmail.com>
2023-10-17 20:08:03 -0300

    db init

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-from models.news_bot.news_bot_model import SCRAPPER_MODEL
-from models.news_bot.articles_model import ARTICLE_MODEL
+from models.news_bot.news_bot_model import SCRAPPER_MODEL, SCRAPPING_DATA, BLACKLIST, KEWORDS, SITES
 from models.word_files.word_files_model import FILES_MODEL
+from models.news_bot.articles_model import ARTICLE_MODEL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from pathlib import Path
+import json  
 import os
 
 load_dotenv()
@@ -23,4 +25,47 @@ SCRAPPER_MODEL.metadata.create_all(engine)
 ARTICLE_MODEL.metadata.create_all(engine)
 
 # Export the sql session
-session = Session()  
+session = Session() 
+ 
+
+ROOT_DIRECTORY = Path(__file__).parent.resolve()
+
+print('_________________')
+try: 
+    # Populates the sites and keyword tables
+    if not session.query(SCRAPPING_DATA).first():
+
+        with open(f'{ROOT_DIRECTORY}/models/news_bot/data.json', 'r') as data_file:
+            config = json.load(data_file)
+
+        for item in config:   
+            keyword = item['main_keyword']
+            keywords = item['keywords']
+            sites = item['sites']
+            black_list = item['black_list']
+
+            scrapping_data = SCRAPPING_DATA(main_keyword=keyword.casefold())
+
+            for keyword in keywords:
+                scrapping_data.keywords.append(KEWORDS(keyword=keyword.casefold()))
+
+            for word in black_list:
+                scrapping_data.blacklist.append(BLACKLIST(black_Word=word.casefold()))
+
+            for site_data in sites:
+                site = SITES(
+                    site=site_data['site'],
+                    base_url=site_data['base_url'],
+                    website_name=site_data['website_name'],
+                    is_URL_complete=site_data['is_URL_complete']
+                )
+                scrapping_data.sites.append(site)
+
+            
+            session.add(scrapping_data)
+
+        print('Initial site data saved to db')
+        session.commit()
+except Exception as e:
+    print(f'An error occurred: {str(e)}')
+
