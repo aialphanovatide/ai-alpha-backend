c1722ce82ffe46e1c56e8b61708c179706d24753
daviddflix <david-972010@hotmail.com>
2023-10-17 12:58:20 -0300

    init of server.py

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
-from flask import Blueprint
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.name = 'AI Alpha'
+CORS(app, origins='*')
+
+if __name__ == '__main__':
+    try:
+        print('---AI Alpha server is running---') # Once the server is ready. Add a pin message to slack
+        app.run(threaded=True, debug=False, port=7000, use_reloader=False) 
+    except Exception as e:
+        print(f"Failed to start the AI Alpha server: {e}")      
+
+print('---AI Alpha server was stopped---')
