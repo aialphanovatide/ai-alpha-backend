6258070383c64a9561d2ba7a9176ae916e06d68d
daviddflix <david-972010@hotmail.com>
2023-10-23 12:51:53 -0300

    merging

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ import os
 
 load_dotenv()
 
+news_bot_start_time = 1
+
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,10 @@
-from config import db_url
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_MAX_INSTANCES, EVENT_JOB_EXECUTED
 from routes.slack.templates.poduct_alert_notification import send_notification_to_product_alerts_slack_channel
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_MAX_INSTANCES, EVENT_JOB_EXECUTED
+from apscheduler.schedulers.background import BackgroundScheduler
+from routes.news_bot.scrapper import start_periodic_scraping
+from models.news_bot.news_bot_model import SCRAPPING_DATA
+from config import session
+from config import db_url, news_bot_start_time
 
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore('sqlalchemy', url= db_url)
@@ -9,7 +12,7 @@ if scheduler.state != 1:
     print('Scheduler started')
     scheduler.start()
 
-
+news_bot_start_time = 1 
 def job_executed(event): # for the status 200 of the bot
     print(f'{event.job_id} was executed successfully at {event.scheduled_run_time}, response: {event.retval}')
 
@@ -22,20 +25,31 @@ def job_error(event): # for the status with an error of the bot
 
 def job_max_instances_reached(event): # for the status with an error of the bot
     job_id = str(event.job_id).capitalize()
-   
-    message = f'Maximum number of running instances reached, consider upgrading the time interval'
+    message = f'Maximum number of running instances reached'
     # send_notification_to_product_alerts_slack_channel(title_message=f'{job_id} News Bot - Execution error', 
                                                     #   sub_title="Response:", 
                                                     #   message=message)
-    job = scheduler.get_job(job_id=job_id)
-    print('job > ', job)
-    scheduler.remove_job(job_id=job_id)
-    new_job = scheduler.add_job(job)
-    print('new_job > ', new_job)
-    # send_notification_to_product_alerts_slack_channel(title_message=f'{job_id} News Bot restarted', 
-    #                                                   sub_title="Response:", 
-    #                                                   message=f"Execution of {job_id} News Bot restarted successfully, consider upgrading the time interval")
-    print(f'{job_id}: {message}')
+    try:
+        target = event.job_id
+        scheduler.remove_job(job_id=target)
+        
+        scrapping_data_objects = session.query(SCRAPPING_DATA).filter(SCRAPPING_DATA.main_keyword == target.casefold()).all()
+        main_keyword = scrapping_data_objects[0].main_keyword
+
+        job = scheduler.add_job(start_periodic_scraping, 'interval', minutes=(news_bot_start_time + 5), id=target, replace_existing=True, args=[main_keyword], max_instances=1)
+        if job:
+            # send_notification_to_product_alerts_slack_channel(title_message=f'{job_id} News Bot restarted', 
+            #                                                   sub_title="Response:", 
+            #                                                   message=f"Execution of {job_id} News Bot restarted successfully")
+            print(f"""{job_id} News Bot restarted\n
+                    An interval of *{news_bot_start_time + 5} Minutes* has been set for scrapping data')""")
+    except Exception as e:
+        print(f'Error while restarting {job_id} News Bot\n{str(e)}')
+        # send_notification_to_product_alerts_slack_channel(title_message=f'Error while restarting {job_id} News Bot', 
+        #                                                   sub_title="Response:", 
+        #                                                   message=f"str(e)")
+   
+
    
 scheduler.add_listener(job_error, EVENT_JOB_ERROR)
 scheduler.add_listener(job_max_instances_reached, EVENT_JOB_MAX_INSTANCES)
