258e8794f6bbe4f3aed146d57bef5e538e62989e
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2023-11-08 11:47:03 -0300

    sites added, timeout fixed

--- routes/news_bot/sites/criptonews.py
+++ routes/news_bot/sites/criptonews.py
@@ -1,73 +1,87 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-from routes.news_bot.validations import validate_content, title_in_blacklist
 
+from routes.news_bot.validations import title_in_blacklist, validate_content
 
 def validate_date_cryptonews(date_text):
-    if "m" in date_text or "h" in date_text:
-        try:
-            time_unit = 1 if "m" in date_text else 60
-            time_value = int(date_text.split()[0])
-            date = datetime.now() - timedelta(minutes=time_unit * time_value)
-            return date
-        except ValueError:
-            pass
-    return None
+    try:
+        if 'm' in date_text:
+            # Si la fecha está en minutos
+            minutes_ago = int(date_text.split()[0])
+            current_time = datetime.now()
+            article_date = current_time - timedelta(minutes=minutes_ago)
+        else:
+            # Si la fecha está en un formato diferente, puedes adaptarlo según el caso
+            article_date = None
 
-def extract_image_url_cryptonews(html):
-    image_div = html.find('div', class_='detail-image-wrap')
-    if image_div:
-        style = image_div.get('style')
+        if article_date and (current_time - article_date) < timedelta(days=1):
+            return article_date.strftime('%Y-%m-%d')
+        else:
+            return None
+    except Exception:
+        return None
+
+def extract_image_urls_cryptonews(html):
+    image_urls = []
+    base_url = "https://cnews24.ru/uploads/"
+
+    soup = BeautifulSoup(html, 'html.parser')
+    image_divs = soup.find_all('div', class_='detail-image-wrap')
+
+    for div in image_divs:
+        style = div.get('style')
         if style:
-            url_start = style.find('url(')
-            url_end = style.find(')', url_start)
-            if url_start != -1 and url_end != -1:
-                image_url = style[url_start + 4 : url_end]
-                return image_url
-    return None
+            url_start = style.find('(') + 1
+            url_end = style.find(')')
+            image_url = style[url_start:url_end]
+            if image_url.startswith(base_url):
+                image_urls.append(image_url)
 
+    return image_urls
 
 def extract_article_content_cryptonews(html):
+    title_element = html.find('h1')
+    if title_element:
+        title = title_element.text.strip()
+    else:
+        title = None
+
     content = ""
-    h1_tags = html.find_all('h1')
-    h2_tags = html.find_all('h2')
-    p_tags = html.find_all('p')
-    for tag in h1_tags + h2_tags + p_tags:
-        content += tag.text.strip()
-    return content.casefold()
+    content_tags = html.find_all(['h2', 'p'])
+    for tag in content_tags:
+        content += tag.text.strip() + '\n'
+
+    return title, content
 
 def validate_cryptonews_article(article_link, main_keyword):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+        'User-Agent': 'Your User-Agent String Here'
     }
 
     try:
         article_response = requests.get(article_link, headers=headers)
-        article_content_type = article_response.headers.get("Content-Type", "").lower()
+        if article_response.status_code != 200:
+            return None, None, None, None
 
-        if article_response.status_code == 200 and 'text/html' in article_content_type:
-            html = BeautifulSoup(article_response.text, 'html.parser')
+        html = BeautifulSoup(article_response.text, 'html.parser')
 
-            # Extract date
-            date_element = html.find('span', class_='datetime')
-            date_text = date_element.text.strip() if date_element else None
+        date_element = html.find('span', class_='datetime flex middle-xs')
+        if date_element:
+            date_text = date_element.text.strip()
             valid_date = validate_date_cryptonews(date_text)
+        else:
+            print('Error: Date not found')
+            valid_date = None
 
-            # Extract article content
-            content = extract_article_content_cryptonews(html)
+        image_urls = extract_image_urls_cryptonews(article_response.text)
+        title, content = extract_article_content_cryptonews(html)
 
-            # Extract image URL
-            image_url = extract_image_url_cryptonews(html)
+        is_title_in_blacklist = title_in_blacklist(title)
+        content_validation = validate_content(main_keyword, content)
 
-            # Validate title, content, and date
-            title_element = html.find('h1')
-            title = title_element.text.strip() if title_element else None
-            is_title_in_blacklist = title_in_blacklist(title)
-            content_validation = validate_content(main_keyword, content)
-
-            if valid_date and content and title and not is_title_in_blacklist and content_validation:
-                return content, valid_date, image_url
+        if valid_date and content and title and not is_title_in_blacklist and content_validation:
+                return content, valid_date, image_urls
     except Exception as e:
         print("Error in CryptoNews:", str(e))
 
