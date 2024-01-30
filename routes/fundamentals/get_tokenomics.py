01e574b8c9d75b906aef8431b82a4070881ba001
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-01-30 18:17:08 -0300

    get tokenomics fix

--- routes/fundamentals/get_tokenomics.py
+++ routes/fundamentals/get_tokenomics.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from config import Tokenomics, Session
+from config import Tokenomics, Token_distribution, Token_utility, Value_accrual_mechanisms, Session
 from sqlalchemy.orm import joinedload
 
 get_coin_bot_tokenomics = Blueprint('getCoinBotTokenomics', __name__)
@@ -11,20 +11,52 @@ def get_tokenomics(coin_bot_id):
             # Realizar una consulta para obtener los tokenomics del Coin Bot específico
             tokenomics = (
                 session.query(Tokenomics)
-                .filter_by(coin_bot_id=coin_bot_id)
                 .options(joinedload(Tokenomics.coin_bot))
+                .filter_by(coin_bot_id=coin_bot_id) 
                 .first()
             )
 
             if tokenomics:
-                # Devolver la información de los tokenomics en formato JSON
+                # Obtener información de token_distribution
+                token_distribution_info = (
+                    session.query(Token_distribution)
+                    .filter_by(coin_bot_id=coin_bot_id)
+                    .first()
+                )
+
+                # Obtener información de token_utility
+                token_utility_info = (
+                    session.query(Token_utility)
+                    .filter_by(coin_bot_id=coin_bot_id)
+                    .first()
+                )
+
+                # Obtener información de value_accrual_mechanisms
+                value_accrual_mechanisms_info = (
+                    session.query(Value_accrual_mechanisms)
+                    .filter_by(coin_bot_id=coin_bot_id)
+                    .first()
+                )
+
+                # Devolver la información de los tokenomics, token_distribution, token_utility y value_accrual_mechanisms en formato JSON
                 return jsonify({
                     'coinBotInfo': {
                         'totalSupply': tokenomics.total_supply,
                         'circulatingSupply': tokenomics.circulating_supply,
-                        'percentCirculatingSupply': tokenomics.percent_circulating_supply,
+                        'percentCirculatingSupply': tokenomics.percentage_circulating_supply,
                         'maxSupply': tokenomics.max_supply,
                         'supplyModel': tokenomics.supply_model,
+                        'tokenDistribution': {
+                            'holderCategory': token_distribution_info.holder_category,
+                            'percentageHeld': token_distribution_info.percentage_held
+                        },
+                        'tokenUtility': {
+                            'gasFeesAndTransactionSettlement': token_utility_info.gas_fees_and_transaction_settlement
+                        },
+                        'valueAccrualMechanisms': {
+                            'tokenBurning': value_accrual_mechanisms_info.token_burning,
+                            'tokenBuyback': value_accrual_mechanisms_info.token_buyback
+                        }
                     }
                 })
             else:
