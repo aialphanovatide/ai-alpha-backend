a407cdd2723e534d1a68d6548dda299b3c939864
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-01-30 12:43:45 -0300

    poster gen fix, get s r route modified

--- routes/news_bot/poster_generator.py
+++ routes/news_bot/poster_generator.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import requests
 import json
 import os
-
+import base64
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,21 +13,20 @@ client = OpenAI(
     api_key=OPENAI_API_KEY,
 )
 
+
 def generate_poster_prompt(article):
-    
-    prompt=f'Please generate a DALL-E prompt exactly related to this {article}, no more than 1 line longer'
+    prompt = f'Please generate a DALL-E prompt exactly related to this {article}, no more than 1 line longer'
     response = client.chat.completions.create(
-            model="gpt-4",
-            messages=[ {"role": "system", "content": prompt},
-                       {"role": "user", "content": prompt}],
-            temperature=0.6,
-            max_tokens=1024,
-        )
+        model="gpt-4",
+        messages=[{"role": "system", "content": prompt},
+                  {"role": "user", "content": prompt}],
+        temperature=0.6,
+        max_tokens=1024,
+    )
     final_prompt = response.choices[0].message.content
 
     api_url = 'https://api.openai.com/v1/images/generations'
-    
-    
+
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {OPENAI_API_KEY}'
@@ -41,18 +40,12 @@ def generate_poster_prompt(article):
 
     response = requests.post(api_url, headers=headers, data=json.dumps(data))
 
-
     if response.status_code == 200:
         result = response.json()
-        return result['data'][0]['url']
+        image_url = result['data'][0]['url']
+        image_data = base64.b64encode(requests.get(image_url).content).decode('utf-8')
+        return image_data
     else:
         print("Error:", response.status_code, response.text)
         return 'No Image'
-
-    
-    
-    
-        
-
-
 
