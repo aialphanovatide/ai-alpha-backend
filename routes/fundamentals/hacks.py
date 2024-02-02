2f71cca2bab23e0915941c6cb8feeea6253044c7
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-02-02 18:04:20 -0300

    hacks working.

--- routes/fundamentals/hacks.py
+++ routes/fundamentals/hacks.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from config import Session
+from config import Session, session
 from config import Hacks
 
 hacks_bp = Blueprint('hacksRoutes', __name__)
@@ -8,17 +8,12 @@ hacks_bp = Blueprint('hacksRoutes', __name__)
 def get_hacks():
     try:
         coin_bot_id = request.args.get('coin_bot_id')
-        if coin_bot_id is None or not coin_bot_id.isdigit():
-            return {'error': 'Coin is required and must be a non-empty string containing only digits'}, 400
-
-        session = Session()
         hacks_data = session.query(Hacks).filter_by(coin_bot_id=coin_bot_id).all()
-
         if not hacks_data:
             return {'error': 'No data found for the requested coin'}, 404
 
         hacks_list = [hack.as_dict() for hack in hacks_data]
-        return {'hacks': hacks_list}, 200
+        return jsonify({'message': hacks_list}), 200
     except Exception as e:
         return {'error': f'Error getting hacks data: {str(e)}'}, 500
 
@@ -27,16 +22,15 @@ def create_hack():
     try:
         data = request.json
         coin_bot_id = data.get('coin_bot_id')
-        
         new_hack = Hacks(
-            hack_name=data.get('hack_name'),
+            hack_name=data.get('hackName'),
             date=data.get('date'),
-            incident_description=data.get('incident_description'),
+            incident_description=data.get('incidentDescription'),
             consequences=data.get('consequences'),
-            mitigation_measure=data.get('mitigation_measure'),
+            mitigation_measure=data.get('mitigationMeasure'),
             coin_bot_id=coin_bot_id
         )
-
+        
         session = Session()
         session.add(new_hack)
         session.commit()
