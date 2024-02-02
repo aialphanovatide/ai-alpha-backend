f02d5bf13d3ba8b58c092205fbeb8e95b2bd3076
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-02-02 17:02:40 -0300

    revenue models routes added

--- routes/fundamentals/revenue_model.py
+++ routes/fundamentals/revenue_model.py
@@ -1,28 +1,44 @@
-from config import Revenue_model, session
-from flask import Blueprint, request
+from config import Revenue_model, Session, session
+from flask import Blueprint, jsonify, request
 
 
-revenue_model = Blueprint('revenue_model_bp', __name__)
+revenue_model_bp = Blueprint('revenue_model_bp', __name__)
 
-@revenue_model.route('/revenue_model', methods=['POST'])
+@revenue_model_bp.route('/api/create_revenue_model', methods=['POST'])
 def post_revenue_model():
-
     try:
-        data = request.data
-        coin_bot_id = data['coin_bot_id']
-        analized_revenue = data['analized_revenue']
-        fees_1ys = data['fees_1ys']
+        data = request.json
+        print(data)
+        coin_bot_id = data.get('coin_bot_id')
 
-        if analized_revenue or fees_1ys:
-            new_revenue = Revenue_model(analized_revenue=analized_revenue,
-                          fees_1ys=fees_1ys,
-                          coin_bot_id=coin_bot_id
-                          )
+        new_revenue = Revenue_model(
+            analized_revenue=data.get('analized_revenue'),
+            fees_1ys=data.get('fees_1ys'),
+            coin_bot_id=coin_bot_id
+        )
+        with Session() as session:
             session.add(new_revenue)
             session.commit()
-
-        return 'Revenue model updated', 200
-
+        return jsonify({'message': 'Content created successfully'}), 200
         
     except Exception as e:
-        return f'Error posting a new revenue model: {str(e)}', 500
+        return {'error': f'Error creating content: {str(e)}'}, 500
+
+
+@revenue_model_bp.route('/api/get_revenue_models', methods=['GET'])
+def get_revenue_models():
+    try:
+        coin_bot_id = request.args.get('coin_bot_id')
+        print(coin_bot_id)
+        if coin_bot_id is None:
+            return 'Coin is required and must be a non-empty string containing only digits', 400
+
+        revenue_models = session.query(Revenue_model).filter_by(coin_bot_id=coin_bot_id).all()
+        print('revenue_models',revenue_models)
+        
+        revenue_models_list = [revenue_model.as_dict() for revenue_model in revenue_models]
+        print('revenue_models_list', revenue_models_list)
+        
+        return {'revenue_models': revenue_models_list}, 200
+    except Exception as e:
+        return f'Error getting revenue models: {str(e)}', 500
