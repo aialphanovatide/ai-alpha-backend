1d7edaf235ba9e52e8c552c3300e478c74ef64d8
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-02-07 17:50:40 -0300

    edit routes added

--- routes/fundamentals/hacks.py
+++ routes/fundamentals/hacks.py
@@ -38,3 +38,26 @@ def create_hack():
         return {'message': f'Hack for coin_bot_id {coin_bot_id} created successfully'}, 201
     except Exception as e:
         return {'error': f'Error creating hack: {str(e)}'}, 500
+    
+    
+@hacks_bp.route('/api/hacks/edit/<int:hack_id>', methods=['PUT'])
+def edit_hack(hack_id):
+    try:
+        data = request.json
+        hack_to_edit = session.query(Hacks).filter(Hacks.id == hack_id).first()
+
+        if not hack_to_edit:
+            return {'message': 'Hack not found', 'status': 404}, 404
+
+        # Actualizar los campos del hack con los datos recibidos en el body de la solicitud
+        hack_to_edit.hack_name = data.get('hackName', hack_to_edit.hack_name)
+        hack_to_edit.date = data.get('date', hack_to_edit.date)
+        hack_to_edit.incident_description = data.get('incidentDescription', hack_to_edit.incident_description)
+        hack_to_edit.consequences = data.get('consequences', hack_to_edit.consequences)
+        hack_to_edit.mitigation_measure = data.get('mitigationMeasure', hack_to_edit.mitigation_measure)
+
+        session.commit()
+
+        return {'message': f'Hack with ID {hack_id} updated successfully', 'status': 200}, 200
+    except Exception as e:
+        return {'error': f'Error editing hack: {str(e)}', 'status': 500}, 500
