a804ae10b36c9ef37c0cbc74172e163efcc1adae
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-03-11 15:03:15 -0300

    new work flow for news bot

--- routes/news_bot/validations.py
+++ routes/news_bot/validations.py
@@ -4,7 +4,7 @@ import ahocorasick
 
 
 def title_in_db(article_title, session_instance): # True if Title already in DB  
-    try:
+    try:    
         existing_title =session_instance.query(Article).filter(Article.title.ilike(article_title)).first()
 
         return existing_title is not None
@@ -12,8 +12,7 @@ def title_in_db(article_title, session_instance): # True if Title already in DB
     except Exception as e:
         print(f'Error in title_in_db: {str(e)}')
         return False
-
-
+    
 def url_in_db(input_url, session_instance): # True if URL already in DB
     try:
         existing_url = session_instance.query(Article).filter(Article.url.ilike(input_url.casefold().strip())).first()
@@ -23,7 +22,6 @@ def url_in_db(input_url, session_instance): # True if URL already in DB
     except Exception as e:
         print(f'Error in url_in_db: {str(e)}')
         return False
-
 
 def validate_content(bot_name, content, session_instance):
     try:
@@ -56,7 +54,6 @@ def validate_content(bot_name, content, session_instance):
         print(f'Error in validate_content: {str(e)}')
         return False
 
-
 def find_matched_keywords(bot_name, content, session_instance):
     try:
         coin_name = session_instance.query(CoinBot).filter(CoinBot.bot_name == bot_name.casefold()).first()
@@ -84,6 +81,7 @@ def find_matched_keywords(bot_name, content, session_instance):
     except Exception as e:
         print(f'Error in find_matched_keywords: {str(e)}')
         return set()
+
 
 
 def title_in_blacklist(input_title_formatted, session_instance): # True if Title in blacklist
