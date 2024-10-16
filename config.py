755533a19e3c06c61e6103bbc06982aae2580238
daviddflix <daviddflix@gmail.com>
2024-10-16 11:52:03 -0300

    fix: added debug statement in ngrok service in docker

--- config.py
+++ config.py
@@ -1441,13 +1441,6 @@ class Upgrades(Base):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
-session = Session()
-
-
-
-# -------------- NOTIFICATION SCHEMAS ------------
 
 class Topic(Base):
     """
@@ -1514,6 +1507,11 @@ class Notification(Base):
 
     def as_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+session = Session()
 
 
 ROOT_DIRECTORY = Path(__file__).parent.resolve()
