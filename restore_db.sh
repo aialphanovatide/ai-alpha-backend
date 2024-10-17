56dd751ff54a23dee0daeae262089380268b459e
daviddflix <daviddflix@gmail.com>
2024-10-17 14:22:30 -0300

    test db production

--- restore_db.sh
+++ restore_db.sh
@@ -1,3 +1,33 @@
+# #!/bin/bash
+# set -e
+
+# # Function to check if the database is empty
+# is_db_empty() {
+#     local db_name="$1"
+#     local table_count=$(psql -U "$POSTGRES_USER" -d "$db_name" -tAc "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public'")
+#     [ "$table_count" -eq 0 ]
+# }
+
+# # Wait for the database to be ready
+# until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+#     echo "Waiting for database to be ready..."
+#     sleep 2
+# done
+
+# # Check if the database is empty
+# if is_db_empty "$POSTGRES_DB"; then
+#     echo "Database is empty. Restoring from backup..."
+#     if [ -f "$BACKUP_FILE" ]; then
+#         pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" < "$BACKUP_FILE"
+#         echo "Backup restored successfully."
+#     else
+#         echo "Backup file not found: $BACKUP_FILE"
+#         exit 1
+#     fi
+# else
+#     echo "Database is not empty. Skipping backup restore... Using existing database: $POSTGRES_DB"
+# fi
+
 #!/bin/bash
 set -e
 
@@ -9,16 +39,25 @@ is_db_empty() {
 }
 
 # Wait for the database to be ready
-until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+until pg_isready -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
     echo "Waiting for database to be ready..."
     sleep 2
 done
+
+echo "Database is ready. Checking if it's empty..."
 
 # Check if the database is empty
 if is_db_empty "$POSTGRES_DB"; then
     echo "Database is empty. Restoring from backup..."
     if [ -f "$BACKUP_FILE" ]; then
-        pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" < "$BACKUP_FILE"
+        echo "Found backup file: $BACKUP_FILE"
+        if [[ "$BACKUP_FILE" == *.sql ]]; then
+            echo "Restoring from SQL dump..."
+            psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" < "$BACKUP_FILE"
+        else
+            echo "Restoring from custom format dump..."
+            pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" "$BACKUP_FILE"
+        fi
         echo "Backup restored successfully."
     else
         echo "Backup file not found: $BACKUP_FILE"
