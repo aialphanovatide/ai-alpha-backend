d12ea026fc435d7c8d2b551d42acbfb408094750
agustin bustamante <agustinbustamante@Agustins-MBP-2.fibertel.com.ar>
2024-11-04 13:49:57 -0300

    merging

--- alembic/versions/e9b1d398537c_added_relationship_between_the_admin_.py
+++ alembic/versions/e9b1d398537c_added_relationship_between_the_admin_.py
@@ -10,6 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.engine.reflection import Inspector
 
 # revision identifiers, used by Alembic.
 revision: str = 'e9b1d398537c'
