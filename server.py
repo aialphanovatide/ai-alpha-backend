67f86d6a910fa289bd35603ccc83a4f33b596034
agustin bustamante <agustinbustamante@Agustins-MBP-2.fibertel.com.ar>
2024-11-08 11:26:09 -0300

    merging

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import os
 import json
+<<<<<<< HEAD
 import threading  # Importar threading para ejecutar el bot de Discord en un hilo
 from flask import Flask, request
+=======
+from flask import Flask, jsonify, request
+>>>>>>> 3d405f7 (merging)
 from flask_cors import CORS
 from flask_mail import Mail
 from routes.chart.chart import chart_bp
